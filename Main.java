e0f56b930f4c21c49a87e72fdd90ca022f518b24
Amit-2002 <ak8780903@gmail.com>
2023-03-04 14:41:37 +0530

    assignment_1 code

---
+++ Main.java
@@ -0,0 +1,68 @@
+// // question_1 - Take 2 integer values in two variables x and y and print their product.
+
+// public class Main {
+//     public static void main(String[] args) {
+//         int x=8;
+//         int y=10;
+//         System.out.println("Product : " + x*y);
+//     }
+// }
+
+
+// question_2 - Print the ASCII value of character ‘U’.
+
+// public class Main{
+//     public static void main(String[] args) {
+//         int x= 'U';
+//         System.out.println(x); 
+//     }
+// }
+
+
+// question_3 - Write a Java program to take the length and breadth of a rectangle and print its area.
+
+// public class Main {
+//     public static void main(String[] args) {
+//         int length=45;
+//         int breadth=27;
+//         int area=length*breadth;
+//         System.out.println("The length is :" + length);
+//         System.out.println("The breadth is :" + breadth);
+//         System.out.println("The Area is :" + area);
+
+//     }
+// }
+
+
+// question_4 - Write a Java program to calculate the cube of a number
+
+// public class Main{
+//     public static void main(String[] args) {
+//         int A=3;
+//         int cube=A*A*A;
+//         System.out.println(cube);
+//     }
+// }
+
+
+// question_5 Write a Java program to swap two numbers with the help of a third variable.
+
+// public class Main{
+//     public static void main(String[] args) {
+//         int num1 = 3;
+//         int num2 = 5;
+//         System.out.println("num1 before swap :" + num1);
+//         System.out.println("num2 before swap :" + num2);
+        
+//         int temp;// this variable is used to help in swapping
+//         temp = num1;
+//         num1 = num2;
+//         num2 = temp;
+//         System.out.println("num1 after swap :" + num1);
+//         System.out.println("num2 after swap : " + num2);
+//     }
+// }
+
+
+
+
