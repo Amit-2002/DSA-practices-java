6c8c1fb20c98b58505f82f8fb0fb189e049aaa2c
Amit-2002 <ak8780903@gmail.com>
2023-03-19 12:10:29 +0530

    Assignment_3 is added

--- Ass_3 operators/Asmt_3.java
+++ Ass_3 operators/Asmt_3.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Asmt_3{
     public static void main(String[] args) {
 
@@ -8,7 +9,43 @@ public class Asmt_3{
         // System.out.println((((a+8)/3)%5)*5); // 5
 
 
+
+
         // Q2 - Swap two numbers without the use of third variable.
+        Scanner sc = new Scanner(System.in); //input class is created.
+        // System.out.println("Enter first number:");
+        // int a = sc.nextInt();
+
+        // System.out.println("Enter second number:");
+        // int b = sc.nextInt();
+        // System.out.println("before swapping a is:" + a);
+        // System.out.println("before swapping b is:" + b);
+
+        // //logic for swapping
+        // a = a + b; // 10 + 20 = 30
+        // b = a - b; // 30 - 20 = 10
+        // a = a - b; // 30 - 10 = 20
+
+        // System.out.println("after swapping a is: " + a);
+        // System.out.println("after swapping b is: " + b);
+
+
+
+        // Q3 - Write a program to calculate the sum of the digits of a 3-digit number.
+        // System.out.println("Enter a 3-digit number: ");
+        // int a = sc.nextInt();
+        // int sum = 0;
+
+        // while(a != 0){
+        //     sum = sum + (a % 10); // getting most right digit of number using this operater & adding it to sum var.
+        //     a = a / 10; // removing most right digit of number by dividing 10. 
+        // }
+        // System.out.println("SUM OF DIGITS :- " + sum); // printing the sum variable.
+
+
+
+
+
         
  
         
